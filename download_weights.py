17065ef28922eaf960111993f083fbe0929ed4d5
carlo_vogel <carlo@aime.info>
2025-01-30 19:50:27 +0100

    download_weights.py: Changed cli-argument --model to the only positional argument; Improved error handling; Improved print output

--- download_weights.py
+++ download_weights.py
@@ -1,11 +1,13 @@
 import argparse
 from getpass import getpass
 from pathlib import Path
+import sys
 
 
 try:
-    from huggingface_hub import snapshot_download, login
-    from huggingface_hub.errors import GatedRepoError
+    from huggingface_hub import snapshot_download, login, whoami
+    from huggingface_hub.errors import GatedRepoError, HFValidationError, LocalEntryNotFoundError, LocalTokenNotFoundError, RepositoryNotFoundError
+    from requests.exceptions import HTTPError
     HF_HUB_PRESENT = True
 
 except ModuleNotFoundError:
@@ -20,27 +22,30 @@ class ModelDownloader:
     def __init__(self):
         self.args = self.load_flags()
         self.prepare_download_dir()
+        self.user_info = self.get_user_info()
 
 
     def load_flags(self):
         parser = argparse.ArgumentParser()
         parser.add_argument(
-            '-m', '--model', type=str, required=True,
+            'model', type=str,
             help='Model name to download'
         )
         parser.add_argument(
-            '-o', '--download_dir', type=str, required=True,
+            '-o', '--download_dir', type=str,
             help='Download directory'
         )
         parser.add_argument(
             '-wo', '--max_workers', type=int, default=8,
             help='Maximum number of workers for downloading'
         )
-        return parser.parse_args()
+        args = parser.parse_args()
+        args.model = args.model.strip('/')
+        return args
 
 
     def prepare_download_dir(self):
-        download_dir = Path(self.args.download_dir)
+        download_dir = Path(self.args.download_dir or Path.cwd())
         if download_dir.is_dir():
             model_name = self.args.model.split('/')[-1]
             download_dir = download_dir / model_name
@@ -69,20 +74,66 @@ class ModelDownloader:
         try:
             self.start_hf_download()
         except GatedRepoError:
-            hf_token = getpass(f'Access to model {self.args.model} is restricted. You need to login to Huggingface. Enter your Huggingface access token (Hidden): ')
-            login(hf_token)
+            if self.user_info:
+                print(
+                    f'This account has no access to the model {self.args.model}. '
+                    f'Visit https://huggingface.co/{self.args.model} to ask for '
+                    f'access or enter another access token (Hidden): ',
+                    end='',
+                    flush=True
+                )
+            else:
+                print(
+                    f'Access to model {self.args.model} is restricted. '
+                    f'Please enter your Huggingface access token (Hidden): ',
+                    end='',
+                    flush=True
+                )
+            self.hf_login()
             try:
                 self.start_hf_download()
-            except GatedRepoError:
-                exit(f'Access to model {self.args.model} is not permitted on this account.')
+            except GatedRepoError as error:
+                exit(
+                    f'Access to model {self.args.model} is not permitted on this account. '
+                    f'Visit https://huggingface.co/{self.args.model} to ask for access.'
+                )
+        except (HFValidationError, LocalEntryNotFoundError, RepositoryNotFoundError) as error:
+            exit(error)
 
-    
+
+    def hf_login(self):
+        hf_token = getpass('')
+        if hf_token:
+            try:
+                login(hf_token)
+                self.user_info = whoami()  # Checks if the user is logged in
+                print(f'Logged in as {self.user_info.get("name")}')
+            except HTTPError as error:
+                exit(error)
+        else:
+            exit('No access token given! Closing...')
+
+
+    def get_user_info(self):
+        if HF_HUB_PRESENT:
+            try:
+                user_info = whoami()  # Checks if the user is logged in
+                print(f'Logged in as {user_info.get("name")}')
+                return user_info
+            except LocalTokenNotFoundError:
+                print('Not logged in to Hugging face!')
+
+
     def start_hf_download(self):
+        print(f'Start download from https://huggingface.co/{self.args.model}:')
         snapshot_download(
             repo_id=self.args.model,
             max_workers=self.args.max_workers,
             local_dir=self.args.download_dir
         )
+        sys.stdout.flush()
+        print('\nDownload Complete')
+
 
 
 def main():
